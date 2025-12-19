f94552586e558f584489d0c551cd6faf0f95475d
atsoc1993 <atsoc1993@gmail.com>
2025-12-18 19:54:24 -0500

    Added trainModel logic, section to display training results and make predictions with current parameters

--- machine-learning-project/src/App.tsx
+++ machine-learning-project/src/App.tsx
@@ -1,11 +1,17 @@
-import { useState } from "react"
+import { useRef, useState } from "react";
+import axios from 'axios';
 
 export default function App() {
 
   const [features, setFeatures] = useState<(string | number)[][]>([[]]);
   const [inputRefs, setInputRefs] = useState<(HTMLInputElement | null)[]>([])
   const [outputData, setOutputData] = useState<(string | number)[]>([])
-  const [outputDataInputRef, setOutputDataInputRef] = useState<HTMLInputElement | null>(null);
+  const [trainingResult, setTrainingResult] = useState<TrainingResult | undefined>()
+  const outputDataInputRef = useRef<HTMLInputElement | null>(null);
+
+
+  const [prediction, setPrediction] = useState<number | undefined>(undefined)
+  const predictionInputRef = useRef<HTMLInputElement | null>(null);
 
   const returnNumericArrayIfNumeric = (arr: string[]): number[] | string[] => {
     if (arr.every(s => Number.isFinite(Number(s)))) return arr.map(s => Number(s));
@@ -20,14 +26,37 @@ export default function App() {
     return displayableString
   }
 
+  type TrainingResult = {
+    success: boolean
+    message: string
+    weights: number[]
+    biases: number[]
+  }
 
+  const trainModel = async () => {
+    setTrainingResult(undefined);
+    const response = await axios.post('http://localhost:8000/app/train_model', {
+      x_features: features,
+      output_data: outputData
+    })
+    const trainingResult: TrainingResult = response.data;
+    console.log(trainingResult)
+    setTrainingResult(trainingResult);
+  }
 
-  const buttonStyling = "p-1 rounded-2xl px-7 bg-indigo-300 shadow-md shadow-indigo-400"
-  const addFeatureButtonStyling = "p-1 px-4 py-2 rounded-2xl bg-indigo-300 shadow-md shadow-indigo-400"
+  const makePrediction = () => {
+    if (!trainingResult) return;
+    const prediction = Number(predictionInputRef.current?.value) * trainingResult?.weights[0] + trainingResult.biases[0];
+    setPrediction(prediction);
+  }
+
+  const buttonStyling = "p-1 rounded-2xl px-3 bg-indigo-300 shadow-md shadow-indigo-400"
+  const addFeatureButtonStyling = "p-1 px-4 mt-5 py-2 rounded-2xl bg-indigo-300 shadow-md shadow-indigo-400"
+  const inputElementStyling = "bg-slate-200 rounded-2xl shadow-md place-self-center w-2/5 text-center"
 
   return (
     <div className="bg-slate-300 h-full w-full p-10">
-      <div className="bg-gray-300 shadow-lg p-10 flex flex-col justify-evenly place-items-center h-9/10 w-3/4 place-self-center rounded-2xl">
+      <div className="bg-gray-300 shadow-lg p-10 flex flex-col justify-evenly place-items-center h-full w-2/5 place-self-center rounded-2xl">
         <h1 className="text-6xl text-center">Linear/Logistic Regression Job Creator</h1>
         <p className="italic text-sm mt-5 text-center">Separate your array of integers or strings by commas, any spaces will be autoremoved, single/double quotes are not mandatory for strings and will still be interpreted as such.</p>
         {features.map((feature, i) => (
@@ -39,6 +68,7 @@ export default function App() {
               </div>
               :
               <input
+                className={inputElementStyling}
                 ref={el => {
                   if (!el) return;
                   const next = inputRefs
@@ -46,7 +76,7 @@ export default function App() {
                   next[i] = el
                   setInputRefs(next)
                 }}
-                className="bg-slate-200 rounded-2xl shadow-md place-self-center w-2/5" />
+              />
             }
             <div className="w-1/4 flex flex-row justify-evenly">
               {feature.length == 0
@@ -65,29 +95,29 @@ export default function App() {
                     })
                   }}
                 >Set Feature</button>
-                : 
+                :
                 <>
-                <button className={buttonStyling}
-                  onClick={() => {
-                    if (!inputRefs[i]) return;
-                    setFeatures(prev => {
-                      const next = [...prev]
-                      next[i] = []
-                      return next
-                    })
-                  }}
+                  <button className={buttonStyling}
+                    onClick={() => {
+                      if (!inputRefs[i]) return;
+                      setFeatures(prev => {
+                        const next = [...prev]
+                        next[i] = []
+                        return next
+                      })
+                    }}
                   >Edit</button>
                   <button className={buttonStyling}
-                  onClick={() => setFeatures(prev => {
-                    const next = [...prev]
-                    next.splice(i, 1);
-                    const nextInputs = inputRefs
-                    nextInputs.splice(i, 1);
-                    setInputRefs(nextInputs);
-                    return next
-                  
-                  })}>Delete</button>
-                  </>
+                    onClick={() => setFeatures(prev => {
+                      const next = [...prev]
+                      next.splice(i, 1);
+                      const nextInputs = inputRefs
+                      nextInputs.splice(i, 1);
+                      setInputRefs(nextInputs);
+                      return next
+
+                    })}>Delete</button>
+                </>
               }
             </div>
           </div>
@@ -95,8 +125,8 @@ export default function App() {
         <button className={addFeatureButtonStyling}
           onClick={() => setFeatures(prev => [...prev, []])}
         >Add New Feature</button>
-        <div className="flex flex-row mt-5 w-full">
-          <label className="place-self-center w-1/4">Y Data:</label>
+        <div className="flex flex-row mt-10 w-full">
+          <label className="place-self-center w-1/4">Output Data:</label>
 
           {outputData.length > 0 ?
             <div className="mx-auto">
@@ -104,13 +134,10 @@ export default function App() {
             </div>
             :
             <input
-              ref={el => {
-                if (!el) return;
-                if (outputDataInputRef) el.value = outputDataInputRef.value
-                setOutputDataInputRef(el)
-              }
-              }
-              className="bg-slate-200 rounded-2xl shadow-md place-self-center w-2/5" />
+              className={inputElementStyling}
+              ref={outputDataInputRef}
+
+            />
 
 
           }
@@ -120,7 +147,7 @@ export default function App() {
               ?
               <button className={buttonStyling}
                 onClick={() => {
-                  const inputText = outputDataInputRef ? outputDataInputRef.value : undefined
+                  const inputText = outputDataInputRef ? outputDataInputRef.current?.value : undefined
                   let rinsedText = inputText?.replace(' ', '').toLowerCase();;
                   let splitText = rinsedText?.split(',');
                   if (!splitText) return;
@@ -137,6 +164,29 @@ export default function App() {
             }
           </div>
         </div>
+        <button className={addFeatureButtonStyling}
+          onClick={async () => trainModel()}
+        >Train Model and Receive Weights & Biases</button>
+        {trainingResult &&
+          <div className="text-center flex flex-col justify-evenly bg-indigo-200 w-3/4 h-2/5 mt-10 shadow-indigo-300 shadow-xl p-6 rounded-2xl">
+            <p className="underline text-4xl">Training Results</p>
+            <p>Message: {trainingResult.message}</p>
+            <p>Weights: {trainingResult.weights}</p>
+            <p>Biases: {trainingResult.biases}</p>
+            <div className="flex flex-row justify-evenly">
+              <label>Predict a Result:</label>
+              <input
+                ref={predictionInputRef}
+                className={inputElementStyling}
+              ></input>
+              <button
+                className={buttonStyling}
+                onClick={() => makePrediction()}
+              >Predict</button>
+            </div>
+            {prediction && <p>Prediction: {prediction}</p>}
+          </div>
+        }
       </div>
     </div>
   )
